6daf63815cd58dc116bb9f9c7b157396b10443f2
Louis F. Ashfield <louis.ashfield@proton.me>
2025-07-09 15:05:58 -0400

    Suppress `command/example.sh` warnings on macOS

--- command/example.sh
+++ command/example.sh
@@ -3,6 +3,11 @@
 set -e
 
 options="-Wall -Wextra -pedantic -std=c99 -g -fsanitize=address -fsanitize=undefined"
+macos_suppress_options="-Wno-deprecated-declarations -Wno-c11-extensions"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    options="$options $macos_suppress_options"
+fi
 
 gcc "examples/$1.c" optiscope.c -o "$1" $options
 ./"$1"
